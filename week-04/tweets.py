1b61cadd7aff9841544737c7f08d7dabcc7038ec
unknown <xwc1019@gmail.com>
2018-04-04 09:55:54 -0400

    commited messages

--- week-04/tweets.py
+++ week-04/tweets.py
@@ -2,15 +2,17 @@ import jsonpickle
 import tweepy
 import pandas as pd
 
-import os
-os.chdir('week-04')
-
 
 
 from twitter_keys import api_key, api_secret
 
 auth = tweepy.AppAuthHandler(api_key,api_secret)
 api = tweepy.API(auth,wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+
+import os
+os.chdir('week-04')
+
+
 
 def auth(key,secret):
     auth = tweepy.AppAuthHandler(key,secret)
@@ -34,7 +36,7 @@ def get_tweets(
     write = False
   ):
   tweet_count = 0
-  all_tweets = pd.DataFrame()
+  # all_tweets = pd.DataFrame()
   while tweet_count < tweet_max:
     try:
       if (max_id <= 0):
@@ -71,7 +73,7 @@ def get_tweets(
         print("No more tweets found")
         break
       for tweet in new_tweets:
-        all_tweets = all_tweets.append(parse_tweet(tweet), ignore_index = True)
+        # all_tweets = all_tweets.append(parse_tweet(tweet), ignore_index = True)
         max_id = new_tweets[-1].id
       tweet_count += len(new_tweets)
     except tweepy.TweepError as e:
@@ -83,17 +85,19 @@ def get_tweets(
   #     all_tweets.to_json(out_file)
   return all_tweets
 
+
+
 # Set a Lat Lon
-latlng = '42.359416,-71.093993' # Eric's office (ish)
+latlng = '42.357245, -71.062104' # Eric's office (ish)
 # Set a search distance
-radius = '1mi'
+radius = '0.5mi'
 # See tweepy API reference for format specifications
 geocode_query = latlng + ',' + radius
 # set output file location
 file_name = 'data/tweets.json'
 # set threshold number of Tweets. Note that it's possible
 # to get more than one
-t_max = 200
+t_max = 5000
 
 tweets = get_tweets(
   geo = geocode_query,
@@ -117,6 +121,83 @@ def parse_tweet(tweet):
   p['user_id'] = tweet.user.id_str
   p['time'] = str(tweet.created_at)
   return p
+
+  def get_tweets(
+      geo,
+      out_file,
+      search_term = '',
+      tweet_per_query = 100,
+      tweet_max = 150,
+      since_id = None,
+      max_id = -1,
+      write = False
+    ):
+    tweet_count = 0
+    all_tweets = pd.DataFrame()
+    while tweet_count < tweet_max:
+      try:
+        if (max_id <= 0):
+          if (not since_id):
+            new_tweets = api.search(
+              q = search_term,
+              rpp = tweet_per_query,
+              geocode = geo
+            )
+          else:
+            new_tweets = api.search(
+              q = search_term,
+              rpp = tweet_per_query,
+              geocode = geo,
+              since_id = since_id
+            )
+        else:
+          if (not since_id):
+            new_tweets = api.search(
+              q = search_term,
+              rpp = tweet_per_query,
+              geocode = geo,
+              max_id = str(max_id - 1)
+            )
+          else:
+            new_tweets = api.search(
+              q = search_term,
+              rpp = tweet_per_query,
+              geocode = geo,
+              max_id = str(max_id - 1),
+              since_id = since_id
+            )
+        if (not new_tweets):
+          print("No more tweets found")
+          break
+        for tweet in new_tweets:
+          all_tweets = all_tweets.append(parse_tweet(tweet), ignore_index = True)
+          max_id = new_tweets[-1].id
+        tweet_count += len(new_tweets)
+      except tweepy.TweepError as e:
+        # Just exit if any error
+        print("Error : " + str(e))
+        break
+    print (f"Downloaded {tweet_count} tweets.")
+    # if write == True:
+    #     all_tweets.to_json(out_file)
+    return all_tweets
+
+latlng = '40.759102, -73.992055' # Eric's office (ish)
+# Set a search distance
+radius = '3mi'
+# See tweepy API reference for format specifications
+geocode_query = latlng + ',' + radius
+# set output file location
+file_name = 'data/tweets.json'
+# set threshold number of Tweets. Note that it's possible
+# to get more than one
+t_max = 2000
+
+tweets = get_tweets(
+  geo = geocode_query,
+  tweet_max = t_max,
+  write = True,
+  out_file = file_name )
 
 
 df = pd.read_json('path/example_json.json')
